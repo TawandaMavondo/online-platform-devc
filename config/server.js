be4b463a1f5e9f8eb0f98724f19583a659d080c4
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2020-10-12 20:23:14 +0200

    .

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    auth: {
+    auth: { 
       secret: env('ADMIN_JWT_SECRET', 'c4db26d07fa0c8883a1bea274f0f266f'),
     },
   },
